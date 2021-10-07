f0c8fda2ab7a194ede065842c13e58c6b4a6fbca
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2021-10-07 20:41:44 +0300

    finished, not adapted

--- src/js/script.js
+++ src/js/script.js
@@ -100,3 +100,18 @@ const instagramSlider = new Swiper(".instagram-slider",{
     freeMode: true
     
 })
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
